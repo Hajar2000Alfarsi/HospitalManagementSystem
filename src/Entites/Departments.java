1e473660aa93e517f865d538cbc33045e2d2b6ae
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 14:11:56 +0400

    Implement full Constructor

--- src/Entites/Departments.java
+++ src/Entites/Departments.java
@@ -11,5 +11,16 @@ public class Departments {
     private int bedCapacity;
     private int availableBeds;
 
-
+    public Departments(String departmentId, List<Doctors> doctors,
+                       String departmentName, String headDoctorId,
+                       List<Nurses> nurses, int bedCapacity,
+                       int availableBeds) {
+        this.departmentId = departmentId;
+        this.doctors = doctors;
+        this.departmentName = departmentName;
+        this.headDoctorId = headDoctorId;
+        this.nurses = nurses;
+        this.bedCapacity = bedCapacity;
+        this.availableBeds = availableBeds;
+    }
 }
