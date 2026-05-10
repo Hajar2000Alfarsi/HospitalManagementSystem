9da488a4e7c6524ef7b25555a38a6aed97cbe66d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 11:54:31 +0400

    Fields of Person class

--- src/Entites/Person.java
+++ src/Entites/Person.java
@@ -1,4 +1,14 @@
 package Entites;
 
+import java.time.LocalDate;
+
 public class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String honeNumber;
+    private String email;
+    private String address;
+
 }
