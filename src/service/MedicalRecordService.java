f6bb877d3f1efc3ea1d40f6c2ba8cb5588ff519b
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 16:04:26 +0400

    Implement method to add Medical Records

--- src/service/MedicalRecordService.java
+++ src/service/MedicalRecordService.java
@@ -1,4 +1,16 @@
 package service;
 
+import Entites.MedicalRecords;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MedicalRecordService {
+    public static List<MedicalRecords> medicalRecordsList = new ArrayList<>();
+
+    public void addMedicalRecords(MedicalRecords records){
+        medicalRecordsList.add(records);
+        System.out.println("Medical Records Added Successfully");
+    }
+
 }
