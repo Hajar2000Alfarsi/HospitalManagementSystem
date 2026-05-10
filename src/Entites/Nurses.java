1c7d1b82ced810fa2aba0a0f27be2f5bfc33b01e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 13:20:00 +0400

    Implement Contructor

--- src/Entites/Nurses.java
+++ src/Entites/Nurses.java
@@ -1,5 +1,6 @@
 package Entites;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Nurses extends Person{
@@ -9,5 +10,27 @@ public class Nurses extends Person{
     private String qualification;
     private List<Patients> assignedPatients;
 
+    public Nurses(String id, String firstName, String lastName,
+                  LocalDate dateOfBirth, String gender,
+                  String poneNumber, String email, String address,
+                  String departmentId, String nurseId, String shift,
+                  String qualification, List<Patients> assignedPatients) {
+        super(id, firstName, lastName, dateOfBirth, gender, poneNumber,
+                email, address);
+        this.departmentId = departmentId;
+        this.nurseId = nurseId;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = assignedPatients;
+    }
+
+    public Nurses(String departmentId, String nurseId, String shift,
+                  String qualification, List<Patients> assignedPatients) {
+        this.departmentId = departmentId;
+        this.nurseId = nurseId;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = assignedPatients;
+    }
 
 }
