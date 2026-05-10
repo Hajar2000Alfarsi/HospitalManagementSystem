b2f92c820eb5543db1213b50add1d2b6e5bf9858
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 14:10:44 +0400

    Add Fields to Departments class

--- src/Entites/Departments.java
+++ src/Entites/Departments.java
@@ -1,4 +1,15 @@
 package Entites;
 
+import java.util.List;
+
 public class Departments {
+    private String departmentId;
+    private String departmentName;
+    private String headDoctorId;
+    private List<Doctors> doctors;
+    private List<Nurses> nurses;
+    private int bedCapacity;
+    private int availableBeds;
+
+
 }
