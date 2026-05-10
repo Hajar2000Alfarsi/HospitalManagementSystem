1e9202a0497f66423366a2e30a4fdf560fa0f069
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 15:13:32 +0400

    Implement method to add doctor

--- src/service/DoctorService.java
+++ src/service/DoctorService.java
@@ -1,6 +1,7 @@
 package service;
 
 import Entites.Doctors;
+import Entites.Patients;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.List;
 public class DoctorService {
     private static List<Doctors> doctorsList = new ArrayList<>();
 
-
+    public void addDoctor(Doctors doctor) {
+        doctorsList.add(doctor);
+        System.out.println("Doctor Added Successfully");
+    }
 }
