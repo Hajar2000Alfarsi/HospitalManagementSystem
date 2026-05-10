811157c5cef50a817c6b06d96ad03b6542b07e7b
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 12:01:37 +0400

    Set Getter an setter

--- src/Entites/Person.java
+++ src/Entites/Person.java
@@ -24,4 +24,75 @@ public class Person {
         this.email = email;
         this.address = address;
     }
+
+    //empty constructor
+    public Person() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPoneNumber() {
+        return poneNumber;
+    }
+
+    public void setPoneNumber(String poneNumber) {
+        this.poneNumber = poneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
 }
