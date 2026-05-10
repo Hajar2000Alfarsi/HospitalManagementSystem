c7d47f733ca952d2df4e871b420b4e552bdfe569
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 14:25:23 +0400

    Create required classes in service and add list for patients

---
+++ src/service/PatientService.java
@@ -0,0 +1,12 @@
+package service;
+
+import Entites.Patients;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientService {
+    //Static List to store all patients
+    List<Patients> patientsList = new ArrayList<>();
+
+}
