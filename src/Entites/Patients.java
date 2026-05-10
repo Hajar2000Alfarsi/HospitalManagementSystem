8c03d5173385260801ef6a3fb6a354b0994cca49
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 12:15:32 +0400

    Add Patient fields

--- src/Entites/Patients.java
+++ src/Entites/Patients.java
@@ -1,4 +1,15 @@
 package Entites;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Patients extends Person{
+    private String patientId;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private String insuranceId;
+    private List<MedicalRecords> medicalRecords;
+    private List<Appointments> appointments;
 }
