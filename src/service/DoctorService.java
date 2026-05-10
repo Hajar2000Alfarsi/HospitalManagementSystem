f771cb46adb6a11bb648be74afd951829639d426
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 14:51:26 +0400

    List to store all patients

--- src/service/DoctorService.java
+++ src/service/DoctorService.java
@@ -1,4 +1,12 @@
 package service;
 
+import Entites.Doctors;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorService {
+    private static List<Doctors> doctorsList = new ArrayList<>();
+
+
 }
