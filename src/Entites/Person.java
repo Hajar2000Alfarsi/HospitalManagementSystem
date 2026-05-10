d132f96b16dc658517687ba97646e0ca5548ba63
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 11:59:16 +0400

    Full constructor

--- src/Entites/Person.java
+++ src/Entites/Person.java
@@ -7,8 +7,21 @@ public class Person {
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
-    private String honeNumber;
+    private String gender;
+    private String poneNumber;
     private String email;
     private String address;
 
+    public Person(String id, String firstName, String lastName,
+                  LocalDate dateOfBirth, String gender, String poneNumber,
+                  String email, String address){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.poneNumber = poneNumber;
+        this.email = email;
+        this.address = address;
+    }
 }
