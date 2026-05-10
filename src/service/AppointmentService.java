172bfa711288118bec540657300b3a61c9ed0c18
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 16:18:49 +0400

    Implement method to add new appointment

--- src/service/AppointmentService.java
+++ src/service/AppointmentService.java
@@ -1,4 +1,18 @@
 package service;
 
+import Entites.Appointments;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AppointmentService {
+    List<Appointments> appointmentsList = new ArrayList<>();
+
+    public void addAppointments(Appointments appointment) {
+        appointmentsList.add(appointment);
+        System.out.println("appointment Added Successfully");
+    }
+
+
 }
