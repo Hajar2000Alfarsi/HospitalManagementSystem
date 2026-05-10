32a78b0437ac23a99a7642c3c8a846989dae600b
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 14:15:50 +0400

    Implement displayInfo to display department information

--- src/Entites/Departments.java
+++ src/Entites/Departments.java
@@ -85,4 +85,16 @@ public class Departments {
     public void setAvailableBeds(int availableBeds) {
         this.availableBeds = availableBeds;
     }
+
+
+
+    public void displayInfo() {
+        System.out.println("Department ID: " + departmentId);
+        System.out.println("Department Name: " + departmentName);
+        System.out.println("Head Doctor ID: " + headDoctorId);
+        System.out.println("Doctors: " + doctors);
+        System.out.println("Nurses: " + nurses);
+        System.out.println("Bed Capacity: " + bedCapacity);
+        System.out.println("Available Beds: " + availableBeds);
+    }
 }
