dedd9c31ecbc6cef374603eed785004d98e43b94
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 15:33:54 +0400

    Implement List to store nurse info

--- src/service/NurseService.java
+++ src/service/NurseService.java
@@ -1,4 +1,11 @@
 package service;
 
+import Entites.Nurses;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NurseService {
+    private static List<Nurses> nursesList = new ArrayList<>();
+
 }
