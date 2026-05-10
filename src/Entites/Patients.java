bc7a6a607d2976117bd253f6b2e8eb9bd967065c
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 12:18:49 +0400

    Implement Constructor

--- src/Entites/Patients.java
+++ src/Entites/Patients.java
@@ -12,4 +12,33 @@ public class Patients extends Person{
     private String insuranceId;
     private List<MedicalRecords> medicalRecords;
     private List<Appointments> appointments;
+
+    public Patients(String id, String firstName, String lastName,
+                    LocalDate dateOfBirth, String gender,
+                    String poneNumber, String email, String address,
+                    String patientId, List<MedicalRecords> medicalRecords,
+                    List<Appointments> appointments, String insuranceId,
+                    String emergencyContact, LocalDate registrationDate,
+                    List<String> allergies, String bloodGroup) {
+        super(id, firstName, lastName, dateOfBirth, gender, poneNumber, email, address);
+        this.patientId = patientId;
+        this.medicalRecords = medicalRecords;
+        this.appointments = appointments;
+        this.insuranceId = insuranceId;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.allergies = allergies;
+        this.bloodGroup = bloodGroup;
+    }
+
+    public Patients(String patientId, List<MedicalRecords> medicalRecords, List<Appointments> appointments, String insuranceId, String emergencyContact, LocalDate registrationDate, List<String> allergies, String bloodGroup) {
+        this.patientId = patientId;
+        this.medicalRecords = medicalRecords;
+        this.appointments = appointments;
+        this.insuranceId = insuranceId;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.allergies = allergies;
+        this.bloodGroup = bloodGroup;
+    }
 }
