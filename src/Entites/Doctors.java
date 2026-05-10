5cfe7807c8cbeffe7081ac41d9325d3eae23afbc
Hajar <hajar2000alfarsi@gmail.com>
2026-05-10 12:44:26 +0400

    Add Doctor fields

--- src/Entites/Doctors.java
+++ src/Entites/Doctors.java
@@ -1,4 +1,18 @@
 package Entites;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Doctors extends Person{
+    private String doctorId;
+    private String specialization;
+    private String qualification;
+    private int experienceYears;
+    private String departmentId;
+    private double consultationFee;
+    private List<LocalDateTime> availableSlots;
+    private List<Patients> assignedPatients;
+
+
+
 }
