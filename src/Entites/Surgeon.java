6256edd370359282c05fae3d4cbbaf74b0fcff8d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-11 15:21:54 +0400

    Add fields to sergon and create constructor

--- src/Entites/Surgeon.java
+++ src/Entites/Surgeon.java
@@ -1,5 +1,36 @@
 package Entites;
 
-public class Surgeon extends Doctors{
+import java.time.LocalDate;
+import java.util.List;
 
+public class Surgeon extends Doctors{
+    private int surgeriesPerformed;
+    private List<String> surgeryTypes;
+    private boolean operationTheatreAccess;
+
+    public Surgeon(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String poneNumber, String email,
+                   String address, String doctorId, List<String> assignedPatients, List<String> availableSlots, double consultationFee,
+                   String departmentId, int experienceYears, String qualification, String specialization, int surgeriesPerformed,
+                   List<String> surgeryTypes, boolean operationTheatreAccess) {
+        super(id, firstName, lastName, dateOfBirth, gender, poneNumber, email, address, doctorId, assignedPatients, availableSlots, consultationFee,
+                departmentId, experienceYears, qualification, specialization);
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
+    }
+
+    public Surgeon(String doctorId, List<String> assignedPatients, List<String> availableSlots, double consultationFee, String departmentId,
+                   int experienceYears, String qualification, String specialization, int surgeriesPerformed, List<String> surgeryTypes,
+                   boolean operationTheatreAccess) {
+        super(doctorId, assignedPatients, availableSlots, consultationFee, departmentId, experienceYears, qualification, specialization);
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
+    }
+
+    public Surgeon(int surgeriesPerformed, List<String> surgeryTypes, boolean operationTheatreAccess) {
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
+    }
 }
