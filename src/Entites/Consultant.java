baa6f92be8d3ff7542c6db465d612a6e1a30408a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-11 15:41:54 +0400

    Add fields to consultant

--- src/Entites/Consultant.java
+++ src/Entites/Consultant.java
@@ -1,4 +1,9 @@
 package Entites;
 
+import java.util.List;
+
 public class Consultant extends Doctors{
+    private List<String> consultationTypes;
+    private boolean onlineConsultationAvailable;
+    private int consultationDuration;
 }
