6fc10155ae336ec7df0c3a20a31562939f594ca6
Hajar <hajar2000alfarsi@gmail.com>
2026-05-11 16:43:23 +0400

    Fields for InPatient class

--- src/Entites/InPatient.java
+++ src/Entites/InPatient.java
@@ -1,4 +1,14 @@
 package Entites;
 
+import java.time.LocalDate;
+
 public class InPatient extends Patients{
+    private LocalDate admissionDate;
+    private LocalDate dischargeDate;
+    private String roomNumber;
+    private String bedNumber;
+    private String admittingDoctorId;
+    private double dailyCharges;
+
+
 }
