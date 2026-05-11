335edd9eb6c694986e5f11788a78254cb9a09ab1
Hajar <hajar2000alfarsi@gmail.com>
2026-05-11 16:12:21 +0400

    Add fields to GeneralPractitioner class

--- src/Entites/GeneralPractitioner.java
+++ src/Entites/GeneralPractitioner.java
@@ -1,5 +1,8 @@
 package Entites;
 
 public class GeneralPractitioner extends Doctors{
+    private boolean walkinAvailable;
+    private boolean homeVisitAvailable;
+    private boolean vaccinationCertified;
 
 }
