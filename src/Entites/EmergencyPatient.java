b10a9ec6a1e88597b96d36f55e4ec71853a1665a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-12 08:28:20 +0400

    Add fields to EmergencyPatient class

--- src/Entites/EmergencyPatient.java
+++ src/Entites/EmergencyPatient.java
@@ -1,5 +1,10 @@
 package Entites;
 
 public class EmergencyPatient extends InPatient{
+    private String emergencyType;
+    private String arrivalMode;
+    private int triageLevel;
+    private boolean admittedViaER;
+
 
 }
