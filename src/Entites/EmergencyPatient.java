f5281e496890804bf31dca3b8b2982f599b29dab
Hajar <hajar2000alfarsi@gmail.com>
2026-05-12 08:29:44 +0400

    Implement getters and setterds

--- src/Entites/EmergencyPatient.java
+++ src/Entites/EmergencyPatient.java
@@ -43,4 +43,36 @@ public class EmergencyPatient extends InPatient{
     public EmergencyPatient() {
 
     }
+
+    public String getEmergencyType() {
+        return emergencyType;
+    }
+
+    public void setEmergencyType(String emergencyType) {
+        this.emergencyType = emergencyType;
+    }
+
+    public String getArrivalMode() {
+        return arrivalMode;
+    }
+
+    public void setArrivalMode(String arrivalMode) {
+        this.arrivalMode = arrivalMode;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
+    }
+
+    public boolean isAdmittedViaER() {
+        return admittedViaER;
+    }
+
+    public void setAdmittedViaER(boolean admittedViaER) {
+        this.admittedViaER = admittedViaER;
+    }
 }
