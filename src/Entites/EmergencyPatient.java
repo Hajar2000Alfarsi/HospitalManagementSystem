66831ff448fa410daf02dd901c754309bceb2bf0
Hajar <hajar2000alfarsi@gmail.com>
2026-05-12 08:29:17 +0400

    Add constructors

--- src/Entites/EmergencyPatient.java
+++ src/Entites/EmergencyPatient.java
@@ -1,10 +1,46 @@
 package Entites;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class EmergencyPatient extends InPatient{
     private String emergencyType;
     private String arrivalMode;
     private int triageLevel;
     private boolean admittedViaER;
 
+    public EmergencyPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String poneNumber, String email, String address, String patientId, List<MedicalRecords> medicalRecords, List<Appointments> appointments, String insuranceId, String emergencyContact, LocalDate registrationDate, List<String> allergies, String bloodGroup, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges, String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        super(id, firstName, lastName, dateOfBirth, gender, poneNumber, email, address, patientId, medicalRecords, appointments, insuranceId, emergencyContact, registrationDate, allergies, bloodGroup, admissionDate, dischargeDate, roomNumber, bedNumber, admittingDoctorId, dailyCharges);
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
 
+    public EmergencyPatient(String patientId, List<MedicalRecords> medicalRecords, List<Appointments> appointments, String insuranceId, String emergencyContact, LocalDate registrationDate, List<String> allergies, String bloodGroup, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges, String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        super(patientId, medicalRecords, appointments, insuranceId, emergencyContact, registrationDate, allergies, bloodGroup, admissionDate, dischargeDate, roomNumber, bedNumber, admittingDoctorId, dailyCharges);
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
+
+    public EmergencyPatient(LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges, String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        super(admissionDate, dischargeDate, roomNumber, bedNumber, admittingDoctorId, dailyCharges);
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
+
+    public EmergencyPatient(String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
+
+    public EmergencyPatient() {
+
+    }
 }
