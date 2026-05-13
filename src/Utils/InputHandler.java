975bfe57761d52d93b1bcdf169e4d6e493ecf528
Hajar <hajar2000alfarsi@gmail.com>
2026-05-13 14:24:27 +0400

    Implement getStringInput method in inputHandler class

--- src/Utils/InputHandler.java
+++ src/Utils/InputHandler.java
@@ -1,4 +1,25 @@
 package Utils;
 
+import java.util.Scanner;
+
 public class InputHandler {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getStringInput(String prompt) {
+     while (true) {
+         //show message
+         System.out.println(prompt);
+         //take input from user
+         String input = scanner.nextLine();
+
+         //check it null or not
+         if (HelperUtils.isValidString(input)) {
+             return input;
+         }
+         System.out.println("Invalid Input. Try Again");
+     }
+    }
+
+
+
 }
