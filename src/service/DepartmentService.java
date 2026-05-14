0983ac31f27d23305feaf29c3a15f00f14b3958a
Hajar <hajar2000alfarsi@gmail.com>
2026-05-14 08:18:32 +0400

    Implement Input handler in Department servics class

--- src/service/DepartmentService.java
+++ src/service/DepartmentService.java
@@ -4,6 +4,7 @@ import Entites.*;
 import Interface.Manageable;
 import Interface.Searchable;
 import Utils.HelperUtils;
+import Utils.InputHandler;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -25,8 +26,6 @@ public class DepartmentService implements Manageable, Searchable {
         this.doctorService = doctorService;
     }
 
-    Scanner scanner = new Scanner(System.in);
-
     public Departments addDepartment() {
         System.out.println("Add new Department ");
 
@@ -35,18 +34,22 @@ public class DepartmentService implements Manageable, Searchable {
         String departmentId = HelperUtils.generateId("DEP");
         System.out.println("Assigned ID to Department: " + departmentId );
 
-        System.out.println("Enter Department Name: ");
-        String departmentName = scanner.nextLine();
+        /*System.out.println("Enter Department Name: ");
+        String departmentName = scanner.nextLine();*/
+        String departmentName = InputHandler.getStringInput("Enter Department Name: ");
 
-        System.out.println("Enter Head Doctor ID: ");
-        String headDoctorId = scanner.nextLine();
+        /*System.out.println("Enter Head Doctor ID: ");
+        String headDoctorId = scanner.nextLine();*/
+        String headDoctorId = InputHandler.getStringInput("Enter Head Doctor ID: ");
 
-        System.out.println("Enter Bed Capacity: ");
-        int bedCapacity = scanner.nextInt();
+        /*System.out.println("Enter Bed Capacity: ");
+        int bedCapacity = scanner.nextInt();*/
+        int bedCapacity = InputHandler.getIntInput("Enter Bed Capacity: ");
 
-        System.out.println("Enter available Beds: ");
+        /*System.out.println("Enter available Beds: ");
         int availableBeds = scanner.nextInt();
-        scanner.nextLine();
+        scanner.nextLine();*/
+        int availableBeds = InputHandler.getIntInput("Enter Available Beds: ");
 
         Departments departments = new Departments(departmentId,doctors,departmentName,headDoctorId,nurses,bedCapacity,availableBeds);
         return departments;
@@ -59,34 +62,37 @@ public class DepartmentService implements Manageable, Searchable {
             departmentsList.add(addDepartment());
             System.out.println("Department Added Successfully");
 
-            System.out.println("Enter q to exit, press ENTER to continue for more patient");
-            if (scanner.nextLine().equalsIgnoreCase("q")) {
+            String input = InputHandler.getStringInput("Enter q to exit, press ENTER to continue: ");
+
+            if (input.equalsIgnoreCase("q")) {
                 continueFlag = false;
             }
         }
         return departmentsList;
     }
 
-    public boolean editDepartment() {
-
-        System.out.println("Enter Department ID: ");
-        String dId = scanner.nextLine();
-
+    public boolean editDepartment(String departmentId) {
         for (Departments department: departmentsList){
             if (HelperUtils.isNotNull(department.getDepartmentId())
-            && department.getDepartmentId().equals(dId)){
-                System.out.println("Enter Updated Department Name: ");
-                department.setDepartmentName(scanner.nextLine());
+            && department.getDepartmentId().equals(departmentId)){
+                /*System.out.println("Enter Updated Department Name: ");
+                department.setDepartmentName(scanner.nextLine());*/
+                department.setDepartmentName(InputHandler.getStringInput("Enter Updated Department Name: "));
 
-                System.out.println("Updated head doctor ID: ");
-                department.setHeadDoctorId(scanner.nextLine());
+                /*System.out.println("Updated head doctor ID: ");
+                department.setHeadDoctorId(scanner.nextLine());*/
+                department.setHeadDoctorId(InputHandler.getStringInput("Updated Head Doctor ID: "));
 
-                System.out.println("Updated Bed Capacity: ");
-                department.setBedCapacity(scanner.nextInt());
+              /*  System.out.println("Updated Bed Capacity: ");
+                department.setBedCapacity(scanner.nextInt());*/
+                int bedCapacity = InputHandler.getIntInput("Updated Bed Capacity: ");
+                department.setBedCapacity(bedCapacity);
 
-                System.out.println("Updated Available Beds: ");
+               /* System.out.println("Updated Available Beds: ");
                 department.setAvailableBeds(scanner.nextInt());
-                scanner.nextLine();
+                scanner.nextLine();*/
+                int availableBeds = InputHandler.getIntInput("Updated Available Beds: ");
+                department.setAvailableBeds(availableBeds);
 
                 System.out.println("Department updated successfully.");
                 return true;
@@ -98,12 +104,10 @@ public class DepartmentService implements Manageable, Searchable {
         return false;
     }
 
-    public boolean removeDepartment() {
-        System.out.println("Enter department ID: ");
-        String dId = scanner.nextLine();
+    public boolean removeDepartment(String departmentId) {
 
         for (Departments department: departmentsList) {
-            if (department.getDepartmentId().equals(dId)) {
+            if (department.getDepartmentId().equals(departmentId)) {
                 departmentsList.remove(department);
                 System.out.println("department removed successfully");
                 return true;
@@ -184,41 +188,39 @@ public class DepartmentService implements Manageable, Searchable {
                 addDepartments();
             }
             case 2 -> {
-                editDepartment();
+                String dId = InputHandler.getStringInput("Enter Department ID: ");
+
+                editDepartment(dId);
             }
             case 3 -> {
-                removeDepartment();
+                String dId = InputHandler.getStringInput("Enter Department ID: ");
+
+                removeDepartment(dId);
             }
             case 4 -> {
-                System.out.println("Write Department ID: ");
-                String id = scanner.nextLine();
+                String id = InputHandler.getStringInput("Write Department ID: ");
+
                 getDepartmentById(id);
             }
             case 5 -> {
                 displayAllDepartments();
             }
             case 6 -> {
-                System.out.println("Write Doctor ID: ");
-                String doctorId = scanner.nextLine();
-                System.out.println("Write Department ID: ");
-                String departmentId = scanner.nextLine();
+                String doctorId = InputHandler.getStringInput("Write Doctor ID: ");
+                String departmentId = InputHandler.getStringInput("Write Department ID: ");
+
 
                 assignDoctor(doctorId, departmentId);
             }
             case 7 -> {
-                System.out.println("Write Nurse ID: ");
-                String nurseID = scanner.nextLine();
-                System.out.println("Write Department ID: ");
-                String departmentId = scanner.nextLine();
+                String nurseID = InputHandler.getStringInput("Write Nurse ID: ");
+                String departmentId = InputHandler.getStringInput("Write Department ID: ");
 
                 assignNurse(nurseID, departmentId);
             }
             case 8 -> {
-                System.out.println("Write Department ID: ");
-                String departmentId = scanner.nextLine();
-                System.out.println("Write Number of available beds: ");
-                int availableBeds = scanner.nextInt();
-                scanner.nextLine();
+                String departmentId = InputHandler.getStringInput("Write Department ID: ");
+                int availableBeds = InputHandler.getIntInput("Write Number of available beds: ");
 
                 updateBedAvailability(departmentId,availableBeds);
             }
