2f5eeaae0fede97ded537932a12022569ce73f0e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-14 12:46:36 +0400

    Create class for ReportService class and implement dailyAppointmentsReport method

--- src/service/ReportService.java
+++ src/service/ReportService.java
@@ -1,6 +1,17 @@
 package service;
 
+import java.time.LocalDate;
+
 public class ReportService {
+    private AppointmentService appointmentService;
+    private DoctorService doctorService;
+    private DepartmentService departmentService;
+    private MedicalRecordService medicalRecordService;
+
+    public void dailyAppointmentsReport(LocalDate date){
+        appointmentService.displayAppointments(date);
+    }
+
 
 
 }
