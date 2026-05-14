d15fe43a517b8e46d5287a5e79f0e7c5ac71616f
Hajar <hajar2000alfarsi@gmail.com>
2026-05-14 13:29:21 +0400

    Implement doctorPerformanceReport, departmentOccupancyReport, patientStatistics method

--- src/service/ReportService.java
+++ src/service/ReportService.java
@@ -1,5 +1,7 @@
 package service;
 
+import Entites.Departments;
+
 import java.time.LocalDate;
 
 public class ReportService {
@@ -11,6 +13,41 @@ public class ReportService {
     public void dailyAppointmentsReport(LocalDate date){
         appointmentService.displayAppointments(date);
     }
+
+    public void doctorPerformanceReport(String doctorId) {
+        System.out.println("--------Doctor Performance Report---------");
+
+        int appointments = appointmentService.getAppointmentsByDoctorId(doctorId).size();
+        int records = medicalRecordService.getRecordsByDoctorId(doctorId).size();
+
+        System.out.println("Toatal Appointment: " + appointments);
+        System.out.println("Medical Records: " +records);
+    }
+
+    public void departmentOccupancyReport(String departmentId){
+        Departments department = departmentService.getDepartmentById(departmentId);
+
+        System.out.println("------Department Occupancy Report-------------");
+
+        System.out.println("Department: " + department.getDepartmentName());
+        System.out.println("Doctors: " + department.getDoctors().size());
+        System.out.println("Nurses: " + department.getNurses().size());
+        System.out.println("Available beds: " + department.getAvailableBeds());
+    }
+
+    public void patientStatistics(String patientId) {
+        int appointments = appointmentService.getAppointmentsByPatientId(patientId).size();
+        int records = medicalRecordService.getRecordsByPatientId(patientId).size();
+
+        System.out.println("---------Patient Statistics-------------  ");
+        System.out.println("Appointments: " + appointments);
+        System.out.println("Medical Records: " + records);
+    }
+
+    public void emergencyCasesReport(){
+
+    }
+
 
 
 
